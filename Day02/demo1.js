94c87fb386cac95af37d33f668a80adf5a172856
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-01-15 12:49:31 +0530

    Day01 & Day02
    Day01 is only Introduction & Setup
    I shared Day02 Code

---
+++ Day02/demo1.js
@@ -0,0 +1,29 @@
+// console.log("Hello World")
+// console.log('Welcome to Angular Module');
+// console.log(`
+//         hi U all Are Enjoying...
+//     `);
+var a1, a_1, a$1;
+// Data Types 
+// number => int,float,double,long,short
+// string => ''/""
+// boolean => true/false
+// void => neutral data type => it does not point anything
+// we can not store any data type value
+// any => number/string/boolean
+// misec. null & undefined => is used to store value as well as to defined as data type 
+var a = 10;
+// console.log("Value of a is "+a);
+// console.log('Value of a is '+a);
+// console.log(`
+//         value of a is ${a}
+//     `);
+var str;
+// console.log("Value of str is "+str);
+var c;
+c = 2.5;
+console.log("Value of c is " + c);
+c = 'sumit';
+console.log("Value of c is " + c);
+c = true;
+console.log("Value of c is " + c);
---
+++ Day02/demo1.ts
@@ -0,0 +1,56 @@
+
+// console.log("Hello World")
+// console.log('Welcome to Angular Module');
+// console.log(`
+//         hi U all Are Enjoying...
+//     `);
+
+var a1,a_1,a$1
+
+// Data Types 
+// number => int,float,double,long,short
+// string => ''/""
+// boolean => true/false
+// void => neutral data type => it does not point anything
+// we can not store any data type value
+// any => number/string/boolean
+// misec. null & undefined => is used to store value as well as to defined as data type 
+
+// var a:number=10;
+// console.log("Value of a is "+a);
+// console.log('Value of a is '+a);
+// console.log(`
+//         value of a is ${a}
+//     `);
+var str!:string;
+
+// console.log("Value of str is "+str);
+
+var c:any;
+c=2.5;
+// console.log("Value of c is "+c);
+// c='sumit';
+// console.log("Value of c is "+c);
+// c=true;
+// console.log("Value of c is "+c);
+
+var d=78;
+var s;
+
+// Type Assertion
+// - It is used to convert ur data type to specific data type 
+// - It is used for tsc compiler purpose
+// - any/Object/Unknown
+// Angle Bracket syntax 
+var a:any;
+var temp=(<string> a);
+// temp.
+
+
+
+
+
+
+
+
+ 
