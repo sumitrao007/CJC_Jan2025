d5514dfd4eb095a14d8f14b69535571b66a3f814
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-01-21 12:58:55 +0530

    Day06

---
+++ Day06/Day06/demo3.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var demo2_1 = require("./demo2");
+var obj = new demo2_1.Myclass(3, "Spruha", 'Raokhande');
+obj.display();
+console.log("Addition is    " + (0, demo2_1.add)(10, 2));
---
+++ Day06/myfolder/demo4.ts
@@ -0,0 +1,3 @@
+import {Myclass} from '../Day06/demo2';
+let obj=new Myclass(6,'Kiran','Raokhande');
+obj.display();
