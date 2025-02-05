eac494e2ebcc9acc326b8c07b3a0fc5012fb0b4a
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-02-05 13:07:04 +0530

    Day16

---
+++ Day16/custompipe/custompipe.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-custompipe',
+  templateUrl: './custompipe.component.html',
+  styleUrls: ['./custompipe.component.css']
+})
+export class CustompipeComponent {
+
+  strDetails:string="Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam id ducimus dolore esse libero quis earum asperiores quaerat consequuntur cupiditate, labore facere unde porro voluptatum aut soluta, reiciendis quas in?Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam id ducimus dolore esse libero quis earum asperiores quaerat consequuntur cupiditate, labore facere unde porro voluptatum aut soluta, reiciendis quas in?"
+}
---
+++ Day16/inbuiltpipe/inbuiltpipe.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-inbuiltpipe',
+  templateUrl: './inbuiltpipe.component.html',
+  styleUrls: ['./inbuiltpipe.component.css']
+})
+export class InbuiltpipeComponent {
+
+  strData:string='U learn anguLar Modeule fRom sumit Raokhande Trainer';
+
+  mydate=new Date();
+
+}
