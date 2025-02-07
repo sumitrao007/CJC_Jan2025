881ee1680c7948f7cfc5524cfb57e04677b29f01
Raokhande sumit Ashok <raokhande.sumit@gmail.com>
2025-02-07 13:07:31 +0530

    Day18

---
+++ Day18/post/post.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from '../http.service';
+
+@Component({
+  selector: 'app-post',
+  templateUrl: './post.component.html',
+  styleUrls: ['./post.component.css']
+})
+export class PostComponent implements OnInit {
+posts:any[]=[];
+  constructor(private service:HttpService){}
+  ngOnInit(): void {
+   this.getDataFromBackend();
+  }
+
+  getDataFromBackend(){
+    this.service.getData()
+    .subscribe((myresponse:any)=>{
+        console.log(myresponse)
+        this.posts=myresponse;
+    })
+  }
+
+
+}
